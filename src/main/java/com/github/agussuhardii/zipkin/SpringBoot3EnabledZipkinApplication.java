424138d5736c13975d7f63c64a345fa4a025d5f7
agussuhardi <agus.suhardii@gmail.com>
2023-07-12 21:00:10 +0800

    init

---
+++ src/main/java/com/github/agussuhardii/zipkin/SpringBoot3EnabledZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.github.agussuhardii.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3EnabledZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3EnabledZipkinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/agussuhardii/zipkin/SpringBoot3EnabledZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.agussuhardii.zipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3EnabledZipkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
